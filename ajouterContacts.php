60743a5eb05be340c67237cb5bbcd655d13fd3c2
Mehmet Minnet <mehmet.minnet@supdevinci-edu.fr>
2024-11-15 18:20:54 +0100

    update files

--- ajouterContacts.php
+++ ajouterContacts.php
@@ -1,14 +1,3 @@
-<form method="post">
-    <label>
-        nom : <input type="text" name="nom"/>
-        <br/>
-        email : <input type="text" name="email"/>
-        <br/>
-        téléphone : <input type="text" name="telephone"/>
-        <br/>
-    </label>
-    <input type="submit" value="Entrer" />
-</form>
 <?php
 include 'bd.php';
 
@@ -19,3 +8,5 @@ $telephone = $_POST['telephone'];
 $base = connectDB();
 
 $sql = $base->query("INSERT INTO contacts (`nom`,`email`,`telephone`) VALUES ('$nom','$email','$telephone')");
+
+header("Location:index.php");
--- edit.php
+++ edit.php
@@ -1,13 +1,30 @@
-<form method="post" >
+<?php
+include 'bd.php';
+
+$id=$_GET['id'];
+
+$base = connectDB();
+$sql = $base->query("SELECT `nom`,`email`,`telephone` FROM contacts WHERE id=$id");
+$contact = $sql->fetch(PDO::FETCH_ASSOC);
+?>
+<form method="post" action="edit.php?id=<?php echo htmlspecialchars($id); ?>">
     <label>
-        nom : <input type="text" name="nom"/>
+        Nom : <input type="text" name="nom" value="<?php echo htmlspecialchars($contact['nom']); ?>" />
         <br/>
-        email : <input type="text" name="email"/>
+        Email : <input type="text" name="email" value="<?php echo htmlspecialchars($contact['email']); ?>" />
         <br/>
-        téléphone : <input type="text" name="telephone"/>
+        Téléphone : <input type="text" name="telephone" value="<?php echo htmlspecialchars($contact['telephone']); ?>" />
         <br/>
     </label>
-    <input type="submit" value="Entrer" />
+    <input type="submit" value="Mettre à jour" />
     <br/>
 </form>
+
 <?php
+$nom = $_POST['nom'];
+$email = $_POST['email'];
+$telephone = $_POST['telephone'];
+
+$base->query("UPDATE `contacts` SET `nom`='$nom',`email`='$email',`telephone`='$telephone' WHERE id=$id");
+header("Location: index.php");
+?>
